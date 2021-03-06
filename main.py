0245f9651b819370cfb9a9b231ffbf6e73225cd6
ch1llay <64579539+ch1llay@users.noreply.github.com>
2021-03-06 23:46:58 +0300

    Добавлено изменение сказки

--- main.py
+++ main.py
@@ -6,93 +6,82 @@ morph = pymorphy2.MorphAnalyzer(lang="ru")
 
 
 def imen(word):
-    try:
-        return morph.parse(word)[0].inflect({"nomn"}).word
-    except:
-        return word
+    w = morph.parse(word)[0].inflect({"nomn"}).word
+    if not w:w = word
+    return w
 
 
 def rod(word):
-    try:
-        return morph.parse(word)[0].inflect({"gent"}).word
-    except:
-        return word
+    w = morph.parse(word)[0].inflect({"gent"}).word
+    if not w:w = word
+    return w
 
 
 def dat(word):
-    try:
-        return morph.parse(word)[0].inflect({"datv"}).word
-    except:
-        return word
+    w = morph.parse(word)[0].inflect({"datv"}).word
+    if not w:w = word
+    return w
 
 
 def vin(word):
-    try:
-        return morph.parse(word)[0].inflect({"accs"}).word
-    except:
-        return word
+    w = morph.parse(word)[0].inflect({"accs"}).word
+    if not w:w = word
+    return w
 
 
 def tvar(word):
-    try:
-        return morph.parse(word)[0].inflect({"ablt"}).word
-    except:
-        return word
+    w = morph.parse(word)[0].inflect({"ablt"}).word
+    if not w:w = word
+    return w
 
 
 def pred(word):
-    try:
-        return morph.parse(word)[0].inflect({"loct"}).word
-    except:
-        return word
+    w = morph.parse(word)[0].inflect({"loct"}).word
+    if not w:w = word
+    return w
 
 
 def zvat(word):
-    try:
-        return morph.parse(word)[0].inflect({"voct"}).word
-    except:
-        return word
+    w = morph.parse(word)[0].inflect({"voct"}).word
+    if not w:w = word
+    return w
 
 
 def rod2(word):
-    try:
-        return morph.parse(word)[0].inflect({"gen2"}).word
-    except:
-        return word
+    w = morph.parse(word)[0].inflect({"gen2"}).word
+    if not w:w = word
+    return w
 
 
 def vin2(word):
-    try:
-        return morph.parse(word)[0].inflect({"acc2"}).word
-    except:
-        return word
+    w = morph.parse(word)[0].inflect({"acc2"}).word
+    if not w: w = word
+    return w
 
 
 def pred2(word):
-    try:
-        return morph.parse(word)[0].inflect({"loc2"}).word
-    except:
-        return word
+    w = morph.parse(word)[0].inflect({"loc2"}).word
+    if not w: w = word
+    return w
 
 
 def get_rod(word):
-    try:
-        return morph.parse(word)[0].tag.gender
-    except:
-        return "masc"
+    w = morph.parse(word)[0].tag.gender
+    if not w: w = "masc"
+    return w
 
 
-def change_rod(word, rod_masc_femn_neut):
-    try: return morph.parse(word)[0].inflect({rod_masc_femn_neut}).word
-    except:
-        return word
+def change_rod(word, rod_masc_femn_neut, temse_pres_past_ftur=None):
+    w = morph.parse(word)[0].inflect({rod_masc_femn_neut}).word
+    if not w:w = word
+    return w
 
 
-def change_time_verb(word, tense_pres_past_futr):
-    try:
-        morph.parse(word)[0].inflect({tense_pres_past_futr}).word
-    except:
-        return word
+def change_time_verb(word, tense_pres_past_futr, rod):
+    w = morph.parse(word)[0].inflect({tense_pres_past_futr, rod}).word
+    if not w: w = word
+    return w
 
+# word = morph.parse("ложка")[0].inflect({"accs"}).word
+# print(word)
 
-word = "технический"
